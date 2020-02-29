77a1694b8628bd5510246459918b54ec065f9903
Steven Chen Hao Nyeo <limpingstone@gmail.com>
2020-02-29 16:00:59 -0500

    Added rx_tx stub

--- examples/yeenet/rx_tx.c
+++ examples/yeenet/rx_tx.c
@@ -4,10 +4,16 @@
 #include "lora.h"
 #include "rx_tx.h"
 
-bool rx_pkt(struct lora_modem *lora, uint8_t rst_pin, uint8_t cs_pin, uint8_t irq_pin, struct packet *pkt) {
+void send_msg(struct lora_modem *lora, uint8_t rst_pin, uint8_t cs_pin, uint8_t irq_pin, struct message *msg) {
 
 }
 
-bool tx_pkt(struct lora_modem *lora, uint8_t rst_pin, uint8_t cs_pin, uint8_t irq_pin, struct packet *pkt){
-
+struct message* recv_msg(struct lora_modem *lora, uint8_t rst_pin, uint8_t cs_pin, uint8_t irq_pin) {
+    struct message *msg;
+    return msg;
 }
+
+bool msg_rcvd(struct lora_modem *lora, uint8_t rst_pin, uint8_t cs_pin, uint8_t irq_pin) {
+    return true;
+}
+
--- examples/yeenet/rx_tx.h
+++ examples/yeenet/rx_tx.h
@@ -1,11 +1,14 @@
 #pragma once
 
-struct packet {
-    uint8_t dest_addr;
-    uint8_t pkt_type;
+#include <stdbool.h>
+
+struct message {
+
 };
 
-bool rx_pkt(struct lora_modem *lora, uint8_t rst_pin, uint8_t cs_pin, uint8_t irq_pin, struct packet *pkt);
+void send_msg(struct lora_modem *lora, uint8_t rst_pin, uint8_t cs_pin, uint8_t irq_pin, struct message *msg);
 
-bool tx_pkt(struct lora_modem *lora, uint8_t rst_pin, uint8_t cs_pin, uint8_t irq_pin, struct packet *pkt);
+struct message* recv_msg(struct lora_modem *lora, uint8_t rst_pin, uint8_t cs_pin, uint8_t irq_pin);
+
+bool msg_rcvd(struct lora_modem *lora, uint8_t rst_pin, uint8_t cs_pin, uint8_t irq_pin);
 
