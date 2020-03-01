209da4321d4c6b864703b381a66a626a310e9bc8
Steven Chen Hao Nyeo <limpingstone@gmail.com>
2020-02-29 19:22:17 -0500

    Added functionality for deploying received packet to discovery layer

---
+++ examples/yeenet/routing_bstrp.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "lora.h"
+
+#define BRDCAST 0
+#define PKT_LEN 255
+
+typedef enum {
+    DATA,
+    ACK,
+    NAK
+} packet_t;
+
+struct packet {
+    uint8_t dest;
+    uint8_t type;
+    uint8_t payload[PKT_LEN - 2];
+};
+
+uint8_t *msg_to_b(struct packet *pkt);
+
+struct packet *b_to_msg(uint8_t *msg);
+
+struct packet *dply_msg(struct lora_modem *lora);
+
+void load_msg(struct lora_modem *lora, struct packet *pkt);
+
